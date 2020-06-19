40e7084f109b347fee95dd541c827408cee65739
Lucas Brum <lucasleonardosb@gmail.com>
2020-06-19 12:34:14 -0300

    InventoryUpdatev2

--- Assets/Scripts/Characters/Actions/PlayerMovement.cs
+++ Assets/Scripts/Characters/Actions/PlayerMovement.cs
@@ -3,85 +3,108 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Animations.Rigging;
 using Cinemachine;
-using UnityEngine.Rendering.PostProcessing;
-using UnityEngine.UI;
-using UnityEngine.UIElements;
 
 public class PlayerMovement : MonoBehaviour
 {
-    Animator anim;
-    public Rig rig;
-    Vector2 input;
+    Animator anim; //componente de animação.
+    public Rig rig;//componente de Rig 
+    Vector2 input;//input para ser usado na movimentação
     public CinemachineCameraOffset m_cineMachineOffSetsConfigs; //classe que controla a posicao da FreeLock camera do cinemachine.
-    public GameObject panelIn;
-    public GameObject imagePanel;
-    bool withInve = false;
+    public GameObject imagePanel;//inventario
+    bool away = false;// o jogador esta com o inventario aberto?
 
-    //float aimDuration = 0.3f;
+    public float turnSpeed = 15; //velocidade de rotacao da yawcamera
+    public Cinemachine.CinemachineFreeLook cinemachine_m; //componente de cinemachine
+    Camera mainCamera; //main camera
 
     private void Awake()
     {
         anim = GetComponent<Animator>();//pegar componente desse objeto.
     }
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        mainCamera = Camera.main; //colocando a man camera na referencia
+
+        cinemachine_m.m_XAxis.m_InvertInput = false; //deixando o InvertInput false, poderia ter feito pelo Inspector mas estava bugado, acredito que seja por conta do componente ainda estar em testes.
+        cinemachine_m.m_YAxis.m_InvertInput = true;
+    }
     private void Update()
     {
-        input.x = Input.GetAxis("Horizontal"); //recebe o valor dos parametros da unity de horizontal e vertical.
-        input.y = Input.GetAxis("Vertical");
+        CharacterMoviment();
+        CharacterView();
+        OpenInventory();
+    }
 
-        anim.SetFloat("InputX", input.x);
-        anim.SetFloat("InputY", input.y); //"conecta" as variaveis do script aos paramentros do animator.
-
-
-        if (Input.GetKey(KeyCode.LeftControl))
-            anim.SetBool("Lower", true); //Segurando o Controll esquerdo a bool Lower do controle de animação recebe true
-        else
-            anim.SetBool("Lower", false); //Soltando o Controll esquerdo a bool Lower do controle de animação recebe false
-
-        if (Input.GetKeyDown(KeyCode.Tab)) 
+    void CharacterMoviment() 
+    {
+        if (away == false)
         {
-            if (withInve)
+            input.x = Input.GetAxis("Horizontal"); //recebe o valor dos parametros da unity de horizontal e vertical.
+            input.y = Input.GetAxis("Vertical");
+
+            anim.SetFloat("InputX", input.x);
+            anim.SetFloat("InputY", input.y); //"conecta" as variaveis do script aos paramentros do animator.
+
+
+            if (Input.GetKey(KeyCode.LeftControl))
+                anim.SetBool("Lower", true); //Segurando o Controll esquerdo a bool Lower do controle de animação recebe true
+            else
+                anim.SetBool("Lower", false); //Soltando o Controll esquerdo a bool Lower do controle de animação recebe false
+
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                withInve = false;
-                panelIn.SetActive(false);
+                anim.SetBool("Jump", true);
+            }
+            else
+            {
+                anim.SetBool("Jump", false);
+            }
+
+            if (rig)
+            {
+                rig.weight = 1.0f;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+            }
+
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+            }
+        }
+    }
+
+    void CharacterView()
+    {
+        
+        float yawCamera = mainCamera.transform.rotation.eulerAngles.y; // Ainda nao entendi o codigo.
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0, yawCamera, 0), turnSpeed * Time.fixedDeltaTime);
+        
+    }
+
+    void OpenInventory()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (away)
+            {
+                away = false;
+                //panelIn.SetActive(false);
                 imagePanel.SetActive(false);
                 UnityEngine.Cursor.visible = false;
                 UnityEngine.Cursor.lockState = CursorLockMode.Locked;
             }
-            else 
+            else
             {
-                withInve = true;
-                panelIn.SetActive(true);
+                away = true;
+                //panelIn.SetActive(true);
                 imagePanel.SetActive(true);
                 UnityEngine.Cursor.visible = true;
                 UnityEngine.Cursor.lockState = CursorLockMode.Confined;
             }
-        }
-
-        /*
-        if (Input.GetMouseButton(1))
-        {
-            rig.weight += Time.deltaTime / aimDuration;
-            m_cineMachineOffSetsConfigs.m_Offset = new Vector3(0.38f, -0.15f, 1);
-        }
-        else
-        {
-            m_cineMachineOffSetsConfigs.m_Offset = new Vector3(0.38f, -0.15f, -0.9f);
-            rig.weight -= Time.deltaTime / aimDuration;
-        }
-
-    */
-
-        if (rig)
-        {
-           rig.weight = 1.0f;  
-        }
-
-        if (Input.GetKeyDown(KeyCode.Mouse0))
-        {
-        }
-
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
         }
     }
 }
